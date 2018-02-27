fd4e9c9f217cbccd67c2ad84cbfaf8153cdd4ba2
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-02-27 19:10:26 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
  # your code here!
+ if block_given?
+    i=0
+    arr = []
+    while i < array.length
+      arr << yield(array[i])
+      i+=1
+    end
+    arr
+  else
+    array
+  end
 end
